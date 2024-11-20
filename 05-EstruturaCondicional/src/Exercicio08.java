24a95c4b1ffc78a9fc114142dcd31b3253e93014
JL Rocha <jl.rocha0799@gmail.com>
2024-11-20 13:17:41 -0300

    Estrutura Condicional Part-2 + Exercicios

--- 05-EstruturaCondicional/src/Exercicio08.java
+++ 05-EstruturaCondicional/src/Exercicio08.java
@@ -7,28 +7,31 @@ public class Exercicio08 {
     double salary, taxes = 0, aux = 0;
     Scanner input = new Scanner(System.in);
     salary = input.nextDouble();
+    aux = salary;
 
     if (salary >= 0 && salary <= 2000) {
       System.out.println("Isento");
     }
     else if (salary > 2000 && salary <= 3000) {
-      salary -= 2000;
-      taxes = salary * 0.08;
-      System.out.printf("R$ %.2f", taxes);
+      aux -= 2000;
+      taxes = aux * 0.08;
+      System.out.printf("R$ %.2f\n", taxes);
     }
     else if (salary > 3000 && salary <= 4500) {
-      //3002.00
-      salary -= 2000;
-      //1002.00
-      aux = salary - 1000;
-      taxes = (salary - aux) * 0.08;
-      taxes += aux * 0.18;
-      System.out.printf("R$ %.2f", taxes);
+      aux -= 2000;
+      taxes = 1000 * 0.08;
+      aux -= 1000;
+      taxes += (aux * 0.18);
+      System.out.printf("R$ %.2f\n", taxes);
     }
     else if(salary > 4500) {
-      salary -= 2000;
-      
+      aux -= 2000;
+      taxes += 1000 * 0.08;
+      aux -= 1000;
+      taxes += 1500 * 0.18;
+      aux -= 1500;
+      taxes += aux * 0.28;
+      System.out.printf("R$ %.2f\n", taxes);
     }
-
   }
 }
