33589c52052fa9c03af995e54ce67adc78fa1cc4
JL Rocha <jl.rocha0799@gmail.com>
2024-12-07 06:44:40 -0300

    POO - Exercicio vetores part1 - Section 10

--- 10-Memoria-Arrays-Listas/src/Application/Program02.java
+++ 10-Memoria-Arrays-Listas/src/Application/Program02.java
@@ -10,15 +10,19 @@ public class Program02 {
     Locale.setDefault(Locale.US);
     Scanner input = new Scanner(System.in);
     int n = input.nextInt();
-    Products02 program[n];
-
-
-    for (int i = 0; i < n; i++) {
-      //new program[i] = input.nextLine();
+    Products02[] vect = new Products02[n];
+    double sum = 0;
+    for(int i = 0; i < vect.length; i++) {
+      input.nextLine();
+      String name = input.nextLine();
+      double price = input.nextDouble();
+      vect[i] = new Products02(name, price);
+      sum += vect[i].getPrice();
     }
 
+    double average = sum / vect.length;
 
-
+    System.out.printf("AVERAGE PRICE = %.2f\n", average);
 
   }
 }
--- 10-Memoria-Arrays-Listas/src/Entities/Products02.java
+++ 10-Memoria-Arrays-Listas/src/Entities/Products02.java
@@ -4,17 +4,21 @@ public class Products02 {
     private String name;
     private double price;
 
-    public void getName(name) {
+    public Products02(String name, double price) {
+      this.name = name;
+      this.price = price;
+    }
+    public void getName(String name) {
       this.name = name;
     }
     public String setName() {
       return name;
     }
 
-    public void getPrice(price) {
+    public void setPrice(double price) {
       this.price = price;
     }
-    public double setPrice() {
+    public double getPrice() {
       return price;
     }
 }
